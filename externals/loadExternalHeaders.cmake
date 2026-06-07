bc8867f0f49b2aba2c0dd0c67eb2db67944775ea
Draginalo <tropicaltisa@gmail.com>
2026-06-06 23:13:50 -0400

    Tried adding model files.

---
+++ externals/loadExternalHeaders.cmake
@@ -0,0 +1,5 @@
+#STB_Image
+set(EXTERNAL_HEADERS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/externals/headers")
+
+add_library(external_headers INTERFACE)
+target_include_directories(external_headers INTERFACE ${EXTERNAL_HEADERS_DIR})
--- externals/stb_image.cmake
+++
@@ -1,5 +0,0 @@
-#STB_Image
-set(STB_IMAGE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/externals/headers")
-
-add_library(stb_image INTERFACE)
-target_include_directories(stb_image INTERFACE ${STB_IMAGE_DIR})
