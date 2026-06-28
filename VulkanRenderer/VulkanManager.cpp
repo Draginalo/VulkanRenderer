e8d982f84d81023ce6f3340c85ec32882b600653
Draginalo <tropicaltisa@gmail.com>
2026-06-28 17:42:20 -0400

    Added correct api version.

--- VulkanRenderer/VulkanManager.cpp
+++ VulkanRenderer/VulkanManager.cpp
@@ -473,7 +473,7 @@ bool VulkanManager::createInstance()
 	appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	appInfo.pEngineName = "VKRen";
 	appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
+	appInfo.apiVersion = VK_API_VERSION_1_3;
 
 	VkInstanceCreateInfo createInfo{};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
